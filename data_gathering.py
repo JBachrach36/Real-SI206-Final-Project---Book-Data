1b598dbd4fcd4dbb0e3cfac6b1c4b71be1913229
JBachrach36 <jbachrach36@gmail.com>
2025-04-08 15:03:29 -0400

    added docstring to praneel's set_up_database implementation

--- data_gathering.py
+++ data_gathering.py
@@ -19,7 +19,24 @@ google_books_key = my_keys.GOOGLE_BOOKS_KEY
 
 # CREATE DATABASE
 def set_up_database(db_name):
-    pass
+    """
+    Sets up a connection to an SQLite database.
+    Creates full path to the database file.
+    Creates the database file if it doesn't exist.
+    Creates a cursor object for executing SQL commands.
+
+    Args:
+        db_name (str): The name of the database file.
+
+    Returns:
+        tuple: A tuple containing the cursor and connection objects.
+    """
+
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(os.path.join(path, db_name))
+    cur = conn.cursor()
+    return cur, conn
+
 
 # CREATE TABLES
 def create_tables(cur, conn):
